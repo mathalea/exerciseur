1fbf3169e6f15332a37195f59993eb48cdf01d10
Rémi Angot <remi.angot@gmail.com>
2023-02-13 14:57:20 +0100

    Possibilité d'ajouter une étape

--- src/exercices/duree.ts
+++ src/exercices/duree.ts
@@ -11,24 +11,33 @@ export default function exerciceDuree(app: HTMLElement) {
   app.append(container)
   const divQuestion = document.createElement('div')
   const divEnonce = document.createElement('div')
+  
   divEnonce.style.marginBottom = '50px'
   container.appendChild(divEnonce)
   divQuestion.style.textAlign = 'center'
+  divQuestion.style.marginBottom = '30px'
   container.appendChild(divQuestion)
+  let numberOfSteps = 1
   const divStart = document.createElement('div')
   const divMiddle = document.createElement('div')
+  const divMiddle2 = document.createElement('div')
   const divEnd = document.createElement('div')
   const divStep1 = document.createElement('div')
   const divStep2 = document.createElement('div')
+  const divStep3 = document.createElement('div')
   const divStepTotal = document.createElement('div')
-  const button = document.createElement('button')
+  const buttonCheck = document.createElement('button')
   const buttonRedo = document.createElement('button')
+  const buttonAddStep = document.createElement('button')
   buttonRedo.innerText = 'Nouvelle question'
   buttonRedo.style.marginRight = '30px'
-  button.innerText = 'Vérifier'
-  button.style.margin = 'auto'
+  buttonCheck.innerText = 'Vérifier'
+  buttonCheck.style.margin = 'auto'
+  buttonAddStep.innerText = 'Ajouter une étape'
+  buttonAddStep.style.marginRight = '30px'
+  container.appendChild(buttonAddStep)
   container.appendChild(buttonRedo)
-  container.appendChild(button)
+  container.appendChild(buttonCheck)
   const divFeedback = document.createElement('div')
   divFeedback.style.display = 'inline'
   divFeedback.style.marginLeft = '20px'
@@ -43,42 +52,88 @@ export default function exerciceDuree(app: HTMLElement) {
   divStepTotal.style.fontWeight = 'bolder'
   divStep1.style.fontWeight = 'bolder'
   divStep2.style.fontWeight = 'bolder'
+  divStep3.style.fontWeight = 'bolder'
   divStep1.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divStep2.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+  divStep3.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divStepTotal.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divMiddle.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+  divMiddle2.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divQuestion.style.display = 'grid'
-  divQuestion.style.gridTemplateColumns = 'repeat(5, 1fr)'
   divQuestion.style.gridTemplateRows = '30px auto 150px'
   divQuestion.style.alignItems = 'center'
   divMiddle.contentEditable = 'true'
+  divMiddle2.contentEditable = 'true'
   divMiddle.style.backgroundColor = 'white'
+  divMiddle2.style.backgroundColor = 'white'
   divStep1.style.backgroundColor = 'white'
   divStep2.style.backgroundColor = 'white'
+  divStep3.style.backgroundColor = 'white'
   divStepTotal.style.backgroundColor = 'white'
   divStart.style.gridColumn = '1'
   divMiddle.style.gridColumn = '3'
-  divEnd.style.gridColumn = '5'
+  divMiddle2.style.gridColumn = '5'
   divStep1.style.gridColumn = '2'
   divStep2.style.gridColumn = '4'
+  divStep3.style.gridColumn = '6'
   divStep1.style.gridRow = '1'
   divStep2.style.gridRow = '1'
+  divStep3.style.gridRow = '1'
   divStart.style.gridRow = '2'
   divMiddle.style.gridRow = '2'
+  divMiddle2.style.gridRow = '2'
   divEnd.style.gridRow = '2'
   divStep1.contentEditable = 'true'
   divStep2.contentEditable = 'true'
+  divStep3.contentEditable = 'true'
   divStepTotal.contentEditable = 'true'
   divStart.style.padding = '10px'
   divMiddle.style.padding = '10px'
+  divMiddle2.style.padding = '10px'
   divEnd.style.padding = '10px'
   divStep1.style.padding = '10px'
   divStep2.style.padding = '10px'
+  divStep3.style.padding = '10px'
   divStepTotal.style.gridRow = '3'
-  divStepTotal.style.gridColumn = '3'
   divQuestion.style.columnGap = '30px'
+  let line1: LeaderLine
+  let line2: LeaderLine
+  let line3: LeaderLine
+  let lineTotal: LeaderLine
 
-  function init(): void {
+  function putDiv(): void {
+    // line1.remove()
+    // line2.remove()
+    // line3.remove()
+    // lineTotal.remove()
+    if (numberOfSteps === 1) {
+      divEnd.style.gridColumn = '5'
+      divStepTotal.style.gridColumn = '3'
+      divQuestion.style.gridTemplateColumns = 'repeat(5, 1fr)'
+    } else {
+      divStepTotal.style.gridColumn = '4'
+      divEnd.style.gridColumn = '7'
+      divQuestion.style.gridTemplateColumns = 'repeat(7, 1fr)'
+    }
+  }
+  
+  function drawLines (): void {
+    if (numberOfSteps === 1) {
+      line1 = new LeaderLine(divStart, divMiddle, { dropShadow: true })
+      line2 = new LeaderLine(divMiddle, divEnd, { dropShadow: true })
+      lineTotal = new LeaderLine(divStart, divEnd, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom', startSocketGravity: 400 })
+    } else {
+      line1 = new LeaderLine(divStart, divMiddle, { dropShadow: true })
+      line2 = new LeaderLine(divMiddle, divMiddle2, { dropShadow: true })
+      line3 = new LeaderLine(divMiddle2, divEnd, { dropShadow: true })
+      lineTotal = new LeaderLine(divStart, divEnd, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom', startSocketGravity: 400 })
+    }
+
+  }
+
+
+
+  function initValues(): void {
     const startHour = new HMS({ hour: randint(2, 21), minute: randint(1, 59) })
     const endHour = new HMS({ hour: startHour.hour + randint(1, 3), minute: randint(1, startHour.minute - 1) })
     divEnonce.innerText = `Un évènement commence à ${startHour.toString()} et se termine à ${endHour.toString()}. Combien de temps a-t-il duré ?`
@@ -86,21 +141,21 @@ export default function exerciceDuree(app: HTMLElement) {
     divEnd.innerText = endHour.toString()
     divStep1.innerText = '+/- ?'
     divStep2.innerText = '+/- ?'
+    divStep3.innerText = '+/- ?'
     divStepTotal.innerText = '+ ?'
     divMiddle.innerText = ''
+    divMiddle2.innerText = ''
     divStep1.style.color = 'black'
     divStep2.style.color = 'black'
     divStepTotal.style.color = 'black'
     divFeedback.innerText = ''
   }
 
-  init()
+  putDiv()
+  initValues()
+  drawLines()
 
-  new LeaderLine(divStart, divMiddle, { dropShadow: true })
-  new LeaderLine(divMiddle, divEnd, { dropShadow: true })
-  new LeaderLine(divStart, divEnd, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom', startSocketGravity: 400 })
-
-  button.addEventListener('click', () => {
+  buttonCheck.addEventListener('click', () => {
     if (
       checkAllStep() ||
       (checkStep(divStart, divStepTotal, divEnd) &&
@@ -115,22 +170,58 @@ export default function exerciceDuree(app: HTMLElement) {
     }
   })
 
-  buttonRedo.addEventListener('click', init)
+  buttonRedo.addEventListener('click', initValues)
 
   divStep1.addEventListener('focusin', () => removeQuestionMark(divStep1))
   divStep2.addEventListener('focusin', () => removeQuestionMark(divStep2))
+  divStep3.addEventListener('focusin', () => removeQuestionMark(divStep3))
   divStepTotal.addEventListener('focusin', () => removeQuestionMark(divStepTotal))
 
   divStep1.addEventListener('focusout', checkAllStep)
   divStep2.addEventListener('focusout', checkAllStep)
+  divStep3.addEventListener('focusout', checkAllStep)
   divStepTotal.addEventListener('focusout', checkAllStep)
   divMiddle.addEventListener('focusout', checkAllStep)
+  divMiddle2.addEventListener('focusout', checkAllStep)
+
+  buttonAddStep.addEventListener('click', () => {
+    if (buttonAddStep.innerText === 'Ajouter une étape') {
+      numberOfSteps = 2
+      divQuestion.appendChild(divStep3)
+      divQuestion.appendChild(divMiddle2)
+      putDiv()
+      line1.remove()
+      line2.remove()
+      lineTotal.remove()
+      drawLines()
+      buttonAddStep.innerText = 'Supprimer une étape'
+    } else {
+      numberOfSteps = 1
+      divStep3.remove()
+      divMiddle2.remove()
+      putDiv()
+      line1.remove()
+      line2.remove()
+      line3.remove()
+      lineTotal.remove()
+      drawLines()
+      buttonAddStep.innerText = 'Ajouter une étape'
+    }
+  })
 
   function checkAllStep(): boolean {
-    checkStep(divStart, divStep1, divMiddle)
-    checkStep(divMiddle, divStep2, divEnd)
-    checkStep(divStart, divStepTotal, divEnd)
-    return checkStep(divStart, divStep1, divMiddle) && checkStep(divMiddle, divStep2, divEnd) && checkStep(divStart, divStepTotal, divEnd)
+    if (numberOfSteps === 1) {
+      checkStep(divStart, divStep1, divMiddle)
+      checkStep(divMiddle, divStep2, divEnd)
+      checkStep(divStart, divStepTotal, divEnd)
+      return checkStep(divStart, divStep1, divMiddle) && checkStep(divMiddle, divStep2, divEnd) && checkStep(divStart, divStepTotal, divEnd)
+    } else {
+      checkStep(divStart, divStep1, divMiddle)
+      checkStep(divMiddle, divStep2, divMiddle2)
+      checkStep(divMiddle2, divStep3, divEnd)
+      checkStep(divStart, divStepTotal, divEnd)
+      return checkStep(divStart, divStep1, divMiddle) && checkStep(divMiddle, divStep2, divMiddle2) && checkStep(divMiddle2, divStep3, divEnd) && checkStep(divStart, divStepTotal, divEnd)
+    }
   }
 
   function removeQuestionMark(element: HTMLDivElement): void {
