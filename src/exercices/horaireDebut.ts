05d635276acff5396c496929ac36743e0f6a21c5
Rémi Angot <remi.angot@gmail.com>
2023-02-14 21:24:28 +0100

    Le temps avance de la gauche vers la droite

--- src/exercices/horaireDebut.ts
+++ src/exercices/horaireDebut.ts
@@ -5,117 +5,146 @@ import HMS from './utils/HMS'
 export default function exerciceHoraireDebut(app: HTMLElement) {
   // On crée le container et on y place les div
   const container = document.createElement('div')
-  container.style.maxWidth = '800px'
+  container.style.maxWidth = '850px'
   container.style.margin = 'auto'
   container.style.marginTop = '50px'
   app.append(container)
   const divQuestion = document.createElement('div')
   const divEnonce = document.createElement('div')
+  
   divEnonce.style.marginBottom = '50px'
   container.appendChild(divEnonce)
   divQuestion.style.textAlign = 'center'
+  divQuestion.style.marginBottom = '30px'
   container.appendChild(divQuestion)
-  const divHour1 = document.createElement('div')
-  const divHour2 = document.createElement('div')
-  const divHour3 = document.createElement('div')
-  const divHour4 = document.createElement('div')
+  let numberOfSteps = 1
+  const divStart = document.createElement('div')
+  const divMiddle = document.createElement('div')
+  const divMiddle2 = document.createElement('div')
+  const divEnd = document.createElement('div')
   const divStep1 = document.createElement('div')
   const divStep2 = document.createElement('div')
   const divStep3 = document.createElement('div')
   const divStepTotal = document.createElement('div')
-  const button = document.createElement('button')
+  const buttonCheck = document.createElement('button')
   const buttonRedo = document.createElement('button')
+  const buttonAddStep = document.createElement('button')
   buttonRedo.innerText = 'Nouvelle question'
   buttonRedo.style.marginRight = '30px'
-  button.innerText = 'Vérifier'
-  button.style.margin = 'auto'
+  buttonCheck.innerText = 'Vérifier'
+  buttonCheck.style.margin = 'auto'
+  buttonAddStep.innerText = 'Ajouter une étape'
+  buttonAddStep.style.marginRight = '30px'
+  container.appendChild(buttonAddStep)
   container.appendChild(buttonRedo)
-  container.appendChild(button)
+  container.appendChild(buttonCheck)
   const divFeedback = document.createElement('div')
   divFeedback.style.display = 'inline'
   divFeedback.style.marginLeft = '20px'
   divFeedback.style.fontWeight = 'bolder'
   container.appendChild(divFeedback)
-  divQuestion.appendChild(divHour1)
-  divQuestion.appendChild(divHour2)
-  divQuestion.appendChild(divHour3)
-  divQuestion.appendChild(divHour4)
+  divQuestion.appendChild(divStart)
+  divQuestion.appendChild(divMiddle)
+  divQuestion.appendChild(divEnd)
   divQuestion.appendChild(divStep1)
   divQuestion.appendChild(divStep2)
-  divQuestion.appendChild(divStep3)
   divQuestion.appendChild(divStepTotal)
   divStepTotal.style.fontWeight = 'bolder'
   divStep1.style.fontWeight = 'bolder'
   divStep2.style.fontWeight = 'bolder'
   divStep3.style.fontWeight = 'bolder'
-  divHour4.style.fontWeight = 'bolder'
   divStep1.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divStep2.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divStep3.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
-  divHour2.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
-  divHour3.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+  divStart.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+  divMiddle.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+  divMiddle2.style.boxShadow = '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
   divQuestion.style.display = 'grid'
-  divQuestion.style.gridTemplateColumns = 'repeat(7, 1fr)'
   divQuestion.style.gridTemplateRows = '30px auto 150px'
   divQuestion.style.alignItems = 'center'
-  divHour2.contentEditable = 'true'
-  divHour2.style.backgroundColor = 'white'
-  divHour3.contentEditable = 'true'
-  divHour3.style.backgroundColor = 'white'
+  divMiddle.contentEditable = 'true'
+  divMiddle2.contentEditable = 'true'
+  divStart.style.backgroundColor = 'white'
+  divMiddle.style.backgroundColor = 'white'
+  divMiddle2.style.backgroundColor = 'white'
   divStep1.style.backgroundColor = 'white'
   divStep2.style.backgroundColor = 'white'
   divStep3.style.backgroundColor = 'white'
-  divHour4.style.backgroundColor = 'white'
-  divHour1.style.gridColumn = '1'
-  divHour2.style.gridColumn = '3'
-  divHour3.style.gridColumn = '5'
-  divHour4.style.gridColumn = '7'
+  divStart.style.gridColumn = '1'
+  divMiddle.style.gridColumn = '3'
+  divMiddle2.style.gridColumn = '5'
   divStep1.style.gridColumn = '2'
   divStep2.style.gridColumn = '4'
   divStep3.style.gridColumn = '6'
   divStep1.style.gridRow = '1'
   divStep2.style.gridRow = '1'
   divStep3.style.gridRow = '1'
-  divHour1.style.gridRow = '2'
-  divHour2.style.gridRow = '2'
-  divHour3.style.gridRow = '2'
-  divHour4.style.gridRow = '2'
+  divStart.style.gridRow = '2'
+  divMiddle.style.gridRow = '2'
+  divMiddle2.style.gridRow = '2'
+  divEnd.style.gridRow = '2'
   divStep1.contentEditable = 'true'
   divStep2.contentEditable = 'true'
   divStep3.contentEditable = 'true'
-  divHour4.contentEditable = 'true'
-  divHour1.style.padding = '10px'
-  divHour2.style.padding = '10px'
-  divHour3.style.padding = '10px'
-  divHour4.style.padding = '10px'
-  divStep1.style.padding = '10px'
-  divStep2.style.padding = '10px'
-  divStep3.style.padding = '10px'
+  divStart.contentEditable = 'true'
+  divStart.style.padding = '5px'
+  divMiddle.style.padding = '5px'
+  divMiddle2.style.padding = '5px'
+  divEnd.style.padding = '5px'
+  divStep1.style.padding = '5px'
+  divStep2.style.padding = '5px'
+  divStep3.style.padding = '5px'
   divStepTotal.style.gridRow = '3'
-  divStepTotal.style.gridColumn = '4'
-  divQuestion.style.columnGap = '20px'
+  divQuestion.style.columnGap = '30px'
+  let line1: LeaderLine
+  let line2: LeaderLine
+  let line3: LeaderLine
+  let lineTotal: LeaderLine
 
-  let startHour: HMS
-  let hour2: HMS
-  let hour3: HMS
-  let endHour: HMS
+  function putDiv(): void {
+    // line1.remove()
+    // line2.remove()
+    // line3.remove()
+    // lineTotal.remove()
+    if (numberOfSteps === 1) {
+      divEnd.style.gridColumn = '5'
+      divStepTotal.style.gridColumn = '3'
+      divQuestion.style.gridTemplateColumns = 'repeat(5, 1fr)'
+    } else {
+      divStepTotal.style.gridColumn = '4'
+      divEnd.style.gridColumn = '7'
+      divQuestion.style.gridTemplateColumns = 'repeat(7, 1fr)'
+    }
+  }
+  
+  function drawLines (): void {
+    if (numberOfSteps === 1) {
+      line1 = new LeaderLine(divMiddle, divStart, { dropShadow: true })
+      line2 = new LeaderLine(divEnd, divMiddle, { dropShadow: true })
+      lineTotal = new LeaderLine(divEnd, divStart, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom' })
+    } else {
+      line1 = new LeaderLine(divMiddle, divStart, { dropShadow: true })
+      line2 = new LeaderLine(divMiddle2, divMiddle, { dropShadow: true })
+      line3 = new LeaderLine(divEnd, divMiddle2, { dropShadow: true })
+      lineTotal = new LeaderLine(divEnd, divStart, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom' })
+    }
 
-  function init(): void {
-    startHour = new HMS({ hour: randint(1, 23), minute: randint(1, 59) })
-    hour2 = new HMS()
-    hour3 = new HMS()
-    endHour = new HMS({ hour: startHour.hour + randint(1, 3), minute: randint(1, startHour.minute - 1) })
-    divEnonce.innerText = `Un évènement se termine à ${endHour.toString()} et a duré ${endHour
-      .substract(startHour)
-      .toString()}. À quelle heure a-t-il commencé ?`
-    divHour1.innerText = endHour.toString()
-    divHour4.innerText = ''
+  }
+
+
+
+  function initValues(): void {
+    const startHour = new HMS({ hour: randint(2, 21), minute: randint(1, 59) })
+    const endHour = new HMS({ hour: startHour.hour + randint(1, 3), minute: randint(1, startHour.minute - 1) })
+    divEnonce.innerText = `Un évènement termine à ${endHour.toString()} et a duré ${endHour.substract(startHour).toString()}. À quelle heure a-t-il commencé ?`
+    divStart.innerText = ''
+    divEnd.innerText = endHour.toString()
     divStep1.innerText = '+/- ?'
     divStep2.innerText = '+/- ?'
     divStep3.innerText = '+/- ?'
-    divStepTotal.innerText = endHour.substract(startHour).toString()
-    divHour2.innerText = ''
-    divHour3.innerText = ''
+    divStepTotal.innerText = '- ' + endHour.substract(startHour).toString()
+    divMiddle.innerText = ''
+    divMiddle2.innerText = ''
     divStep1.style.color = 'black'
     divStep2.style.color = 'black'
     divStep3.style.color = 'black'
@@ -123,15 +152,17 @@ export default function exerciceHoraireDebut(app: HTMLElement) {
     divFeedback.innerText = ''
   }
 
-  init()
+  putDiv()
+  initValues()
+  drawLines()
 
-  new LeaderLine(divHour1, divHour2, { dropShadow: true })
-  new LeaderLine(divHour2, divHour3, { dropShadow: true })
-  new LeaderLine(divHour3, divHour4, { dropShadow: true })
-  new LeaderLine(divHour1, divHour4, { path: 'arc', dropShadow: true, startSocket: 'bottom', endSocket: 'bottom', startSocketGravity: 400 })
-
-  button.addEventListener('click', () => {
-    if (checkStep1() && checkStep2() && checkStartHour()) {
+  buttonCheck.addEventListener('click', () => {
+    if (
+      checkAllStep() ||
+      (checkStep(divStart, divStepTotal, divEnd) &&
+        HMS.fromString(divStep1.innerText).toSeconds() === 0 &&
+        HMS.fromString(divStep2.innerText).toSeconds() === 0)
+    ) {
       divFeedback.innerText = 'Bravo !'
       divFeedback.style.color = 'green'
     } else {
@@ -140,29 +171,59 @@ export default function exerciceHoraireDebut(app: HTMLElement) {
     }
   })
 
-  buttonRedo.addEventListener('click', init)
+  buttonRedo.addEventListener('click', initValues)
 
   divStep1.addEventListener('focusin', () => removeQuestionMark(divStep1))
   divStep2.addEventListener('focusin', () => removeQuestionMark(divStep2))
   divStep3.addEventListener('focusin', () => removeQuestionMark(divStep3))
   divStepTotal.addEventListener('focusin', () => removeQuestionMark(divStepTotal))
 
-  divStep1.addEventListener('focusout', checkStep1)
-  divStep2.addEventListener('focusout', checkStep2)
-  divHour4.addEventListener('focusout', () => {
-    checkStartHour()
-    checkStep3()
+  divStep1.addEventListener('focusout', checkAllStep)
+  divStep2.addEventListener('focusout', checkAllStep)
+  divStep3.addEventListener('focusout', checkAllStep)
+  divStart.addEventListener('focusout', checkAllStep)
+  divMiddle.addEventListener('focusout', checkAllStep)
+  divMiddle2.addEventListener('focusout', checkAllStep)
+
+  buttonAddStep.addEventListener('click', () => {
+    if (buttonAddStep.innerText === 'Ajouter une étape') {
+      numberOfSteps = 2
+      divQuestion.appendChild(divStep3)
+      divQuestion.appendChild(divMiddle2)
+      putDiv()
+      line1.remove()
+      line2.remove()
+      lineTotal.remove()
+      drawLines()
+      buttonAddStep.innerText = 'Supprimer une étape'
+    } else {
+      numberOfSteps = 1
+      divStep3.remove()
+      divMiddle2.remove()
+      putDiv()
+      line1.remove()
+      line2.remove()
+      line3.remove()
+      lineTotal.remove()
+      drawLines()
+      buttonAddStep.innerText = 'Ajouter une étape'
+    }
   })
-  divHour2.addEventListener('focusout', () => {
-    updateHour2()
-    checkStep1()
-    checkStep2()
-  })
-  divHour3.addEventListener('focusout', () => {
-    updateHour3()
-    checkStep2()
-    checkStep3()
-  })
+
+  function checkAllStep(): boolean {
+    if (numberOfSteps === 1) {
+      checkStep(divStart, divStep1, divMiddle)
+      checkStep(divMiddle, divStep2, divEnd)
+      checkStep(divStart, divStepTotal, divEnd)
+      return checkStep(divStart, divStep1, divMiddle) && checkStep(divMiddle, divStep2, divEnd) && checkStep(divStart, divStepTotal, divEnd)
+    } else {
+      checkStep(divStart, divStep1, divMiddle)
+      checkStep(divMiddle, divStep2, divMiddle2)
+      checkStep(divMiddle2, divStep3, divEnd)
+      checkStep(divStart, divStepTotal, divEnd)
+      return checkStep(divStart, divStep1, divMiddle) && checkStep(divMiddle, divStep2, divMiddle2) && checkStep(divMiddle2, divStep3, divEnd) && checkStep(divStart, divStepTotal, divEnd)
+    }
+  }
 
   function removeQuestionMark(element: HTMLDivElement): void {
     element.innerText = element.innerText.replace('+/- ?', '')
@@ -170,49 +231,19 @@ export default function exerciceHoraireDebut(app: HTMLElement) {
     element.style.color = 'black'
   }
 
-  function checkStartHour(): boolean {
-    if (startHour.isEquivalentToString(divHour4.innerText)) {
-      divHour4.style.color = 'green'
-      divStepTotal.style.color = 'green'
+  function checkStep(divStart: HTMLElement, divStep: HTMLElement, divEnd: HTMLElement): boolean {
+    const hourStart = HMS.fromString(divStart.innerText)
+    const hourStep = HMS.fromString(divStep.innerText)
+    const hourEnd = HMS.fromString(divEnd.innerText)
+    if (
+      hourStart.substract(hourEnd).isEqual(hourStep) &&
+      ((hourStep.sign !== '-' && !hourEnd.isGreaterThan(hourStart)) || (hourStep.sign === '-' && !hourStart.isGreaterThan(hourEnd)))
+    ) {
+      if (divStep.innerText !== '+/- ?') divStep.style.color = 'green'
       return true
     } else {
-      divHour4.style.color = 'red'
-      divStepTotal.style.color = 'black'
+      divStep.style.color = 'black'
       return false
     }
-  }
-  function checkStep1(): boolean {
-    if (endHour.substract(hour2).isEquivalentToString(divStep1.innerText) || HMS.fromString(divStep1.innerText).toSeconds() === 0) {
-      if (HMS.fromString(divStep1.innerText).toSeconds() !== 0) divStep1.style.color = 'green'
-      return true
-    } else {
-      divStep1.style.color = 'red'
-      return false
-    }
-  }
-  function checkStep2(): boolean {
-    if (hour2.substract(hour3).isEquivalentToString(divStep2.innerText) || HMS.fromString(divStep2.innerText).toSeconds() === 0) {
-      if (HMS.fromString(divStep2.innerText).toSeconds() !== 0) divStep2.style.color = 'green'
-      return true
-    } else {
-      divStep2.style.color = 'red'
-      return false
-    }
-  }
-  function checkStep3(): boolean {
-    if (startHour.substract(hour3).isEquivalentToString(divStep3.innerText) || HMS.fromString(divStep3.innerText).toSeconds() === 0) {
-      if (HMS.fromString(divStep2.innerText).toSeconds() !== 0) divStep3.style.color = 'green'
-      return true
-    } else {
-      divStep3.style.color = 'red'
-      return false
-    }
-  }
-
-  function updateHour2(): void {
-    hour2 = HMS.fromString(divHour2.innerText)
-  }
-  function updateHour3(): void {
-    hour3 = HMS.fromString(divHour3.innerText)
   }
 }
